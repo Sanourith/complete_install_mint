6e9ae9ee6624c51a00c68905434811b2db97f645
Psowl <owlsanou@gmail.com>
2026-05-20 23:30:58 +0200

    add commands

--- bash_commands.sh
+++ bash_commands.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
-
+#####################################
+## PREPARATION SAUVEGARDE COMPLETE ##
+#####################################
+path_1="/media/sanou/SSD4OWL/"
+path_2="/media/sanou/SanouDDE/"
+rsync -ah --info=progress2 $path_1 $path_2
 
 ##############################
 ## DESINSTALLER UN LOGICIEL ##
 ##############################
-logiciel="opera-gx-stable"
+logiciel="webstorm"
+apt list --installed | grep $logiciel
+dpkg -l | grep -i $logiciel
+
 sudo apt remove $logiciel
 sudo apt autoremove
 sudo apt purge $logiciel
