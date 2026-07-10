4e8c72f5e0633bc2977b1a3d1e8ffcfe748d38ea
Psowl <owlsanou@gmail.com>
2026-07-11 01:34:42 +0200

    adding mullvad install

--- z_resources/2.linux_softwares.sh
+++ z_resources/2.linux_softwares.sh
@@ -211,6 +211,15 @@ EOF
   log_success "Tor Browser ${LATEST_VERSION} installed successfully."
 }
 
+function _install_mullvad() {
+  sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+
+  echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable stable main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+
+  sudo apt update
+  sudo apt install mullvad-browser
+}
+
 function _install_steam() {
   log_info "# Installing STEAM..."
 
